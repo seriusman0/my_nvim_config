b06a8332bfef5dd8866479c7b269c8a799fbf695
serius <seriusman0@gmail.com>
2022-09-28 11:12:41 +0700

    first commit

---
+++ init.lua
@@ -0,0 +1,59 @@
+--[[
+
+Neovim init file
+Maintainer: brainf+ck
+Website: https://github.com/brainfucksec/neovim-lua
+
+--]]
+
+-- Import Lua modules
+require('packer_init')
+require('core/options')
+require('core/autocmds')
+require('core/keymaps')
+require('core/colors')
+require('core/statusline')
+require('plugins/nvim-tree')
+require('plugins/indent-blankline')
+require('plugins/nvim-cmp')
+require('plugins/nvim-lspconfig')
+require('plugins/nvim-treesitter')
+require('plugins/alpha-nvim')
+require('go').setup()
+
+require'lspconfig'.golangci_lint_ls.setup{}
+require'lspconfig'.gopls.setup{}
+
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  resolve_timeout = 800;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = {
+    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+    max_width = 120,
+    min_width = 60,
+    max_height = math.floor(vim.o.lines * 0.3),
+    min_height = 1,
+  };
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    vsnip = true;
+    ultisnips = true;
+    luasnip = true;
+  };
+}
